f7745a0a79461df5289b8cfca88bc34a1547ee6b
Afonso de Mori <me@afonso.dev>
2026-03-06 18:45:09 +0000

    chore: Add fns-cli

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+# fns-cli
+echo "Installing FNS-CLI..."
+go install github.com/afonsodemori/fns-cli@latest
+sudo ln -sf $HOME/.fns-cli/container.bash_profile $HOME/fns-cli.bash_profile
+grep -qxF 'source ~/fns-cli.bash_profile' "$HOME/.bashrc" || printf '\nsource ~/fns-cli.bash_profile\n' >>"$HOME/.bashrc"
+
+# Ensure gemini directory exists and is writable
+mkdir -p /home/vscode/.gemini
+sudo chown -R vscode:vscode /home/vscode/.gemini || true
+
 if ! command -v gemini >/dev/null 2>&1; then
   echo "Installing Gemini CLI..."
   npm install -g @google/gemini-cli@latest
@@ -10,7 +20,3 @@ if ! command -v shfmt &>/dev/null 2>&1; then
   echo "Installing shfmt..."
   go install mvdan.cc/sh/v3/cmd/shfmt@latest
 fi
-
-# Ensure gemini directory exists and is writable
-mkdir -p /home/vscode/.gemini
-sudo chown -R vscode:vscode /home/vscode/.gemini || true
