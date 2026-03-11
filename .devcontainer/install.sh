b37acdc8aa3e6aaca227a3cb55a9dd1dd176559d
Afonso de Mori <me@afonso.dev>
2026-03-11 01:36:05 +0000

    chore: Update devcontainer base image

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -2,8 +2,6 @@
 set -e
 
 # fns-cli
-echo "Installing FNS-CLI..."
-go install github.com/afonsodemori/fns-cli@latest
 sudo ln -sf $HOME/.fns-cli/container.bash_profile $HOME/fns-cli.bash_profile
 grep -qxF 'source ~/fns-cli.bash_profile' "$HOME/.bashrc" || printf '\nsource ~/fns-cli.bash_profile\n' >>"$HOME/.bashrc"
 
@@ -14,9 +12,4 @@ sudo chown -R vscode:vscode /home/vscode/.gemini || true
 if ! command -v gemini >/dev/null 2>&1; then
   echo "Installing Gemini CLI..."
   npm install -g @google/gemini-cli@latest
-fi
-
-if ! command -v shfmt &>/dev/null 2>&1; then
-  echo "Installing shfmt..."
-  go install mvdan.cc/sh/v3/cmd/shfmt@latest
 fi
